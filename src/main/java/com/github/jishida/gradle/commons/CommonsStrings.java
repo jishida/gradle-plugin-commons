a22b8683368785f817686cea85a8e0565dc9c6c7
Junki Ishida <jishida.contact@gmail.com>
2016-12-05 14:24:43 +0900

    Refactored constant strings

---
+++ src/main/java/com/github/jishida/gradle/commons/CommonsStrings.java
@@ -0,0 +1,6 @@
+package com.github.jishida.gradle.commons;
+
+public class CommonsStrings {
+    public static String GROUP_ID = "com.github.jishida.gradle";
+    public static String ARTIFACT_ID = "gradle-plugin-commons";
+}
